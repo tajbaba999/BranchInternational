d72c715d0416f030d779713b23372147086697e3
tajbaba999 <tajbaba153@gmail.com>
2025-01-08 09:22:06 +0530

    Implemented: Sharedpreferences for user auth token

---
+++ app/src/main/java/com/example/branchinternational/util/SharedPreferencesManager.kt
@@ -0,0 +1,26 @@
+package com.example.branchinternational.util
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class SharedPreferencesManager constructor(private  val sharedPreferences: SharedPreferences){
+    companion object{
+        private const val KEY_AUTH_TOKEN = ""
+    }
+    fun saveAuthToken(token : String?){
+        sharedPreferences.edit().apply{
+            if (token != null){
+                putString(KEY_AUTH_TOKEN, token)
+            }else{
+                remove(KEY_AUTH_TOKEN)
+            }
+            apply()
+        }
+    }
+
+
+    fun getAuthToken() : String?  = sharedPreferences.getString(KEY_AUTH_TOKEN, null)
+
+    fun deleteAuthToken() = saveAuthToken(null)
+}
+
