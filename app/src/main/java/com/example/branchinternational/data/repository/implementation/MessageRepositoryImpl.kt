55667e0831233b041c664ca09cfe7a9dad966090
tajbaba999 <tajbaba153@gmail.com>
2025-01-08 09:38:42 +0530

    Add: MessageRepository for message handling

---
+++ app/src/main/java/com/example/branchinternational/data/repository/implementation/MessageRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.branchinternational.data.repository.implementation
+
+import com.example.branchinternational.data.model.Message
+import com.example.branchinternational.data.repository.interfaces.MessageRepository
+import com.example.branchinternational.data.source.remote.ApiService
+import com.example.branchinternational.util.SharedPreferencesManager
+
+class MessageRepositoryImpl constructor(private val apiService: ApiService, private val sharedPreferencesManager: SharedPreferencesManager) : MessageRepository{
+    private fun getAuthToken() : String?{
+        return sharedPreferencesManager.getAuthToken()
+    }
+
+    override suspend fun getMessages(): Result<List<Message>> {
+       return try {
+           val token = getAuthToken() ?: return Result.failure(Exception("No auth token"))
+           Result.success(apiService.getMesssages(token))
+       }catch (e : Exception){
+           Result.failure(e)
+       }
+    }
+
+}
